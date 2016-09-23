e7b58b4a0e051da968ebdc86c05916a409deaa23
Hyjaz Loudin <hyjaz.loudin@gmail.com>
2016-09-23 14:44:26 -0400

    sample login + table

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
-
 @app.route('/')
-def index():
+def home():
 	return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def index():
+	if request.method == 'POST':
+		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+			error = 'invalid credentials. Pleas try again.'
+		else: 
+			return redirect(url_for('reservation'))
+	return render_template('index.html', error='Invalid credentials. Please try again')
+
+@app.route('/reservation')
+def reservation():
+	return render_template('reservation.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
