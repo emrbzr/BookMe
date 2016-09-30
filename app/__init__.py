fad91d21dae017035809027967f7fdad0bfd5973
Hyjaz Loudin <hyjaz.loudin@gmail.com>
2016-09-30 15:07:21 -0400

    structured the repo

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+#create app
+app = Flask(__name__)
+#connect to config.py
+app.config.from_object('config')
+#secret key
+app.secret_key = os.urandom(24)
+#create db instance
+db = SQLAlchemy(app)
+#import models
+from app import models
+#create all tables in models views
+db.create_all()
+#import views
+from app import views, decorators
---
+++ app/views.py
@@ -0,0 +1,45 @@
+from app import app
+from .decorators import *
+from .models import *
+#if 404 error render 404.html 
+@app.errorhandler(404)
+def not_found_error(error):
+	return render_template('404.html')
+
+#if 500 error render 500.html 
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
+
+#login
+@app.route('/login', methods=['GET', 'POST'])
+def index():
+	error = 'Invalid credentials. Please try again'
+	if request.method == 'POST':
+		user = User.query.filter(db.or_(User.username == request.form['username'])).first()
+		password = User.query.filter(db.or_(User.password == request.form['password'])).first()
+		if user:
+			if password:
+				session['logged_in'] = True
+				return redirect(url_for('reservation'))
+			else:
+				return render_template('login.html',error=error)
+		else:
+			return render_template('login.html',error=error)
+	return render_template('login.html')
+
+#logout
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	session.clear()
+	return redirect(url_for('index'))
+
+#reservation
+@app.route('/reservation')
+@login_required 
+@nocache
+def reservation():
+		return render_template('reservation.html')
+	
